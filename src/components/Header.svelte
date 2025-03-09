1953f0899392ae4389a6501d8661a414f55164f3
Chandrashekar <chandrashekar31.v@gmail.com>
2025-03-09 22:14:09 +0530

    css changes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,24 +1,54 @@
 <script>
-  import logo from "../resources/images/logo.jpg"
-  import { goto } from '$app/navigation'
+  import logo from "../resources/images/logo.jpg";
+  import { goto } from "$app/navigation";
 </script>
 
 <div class="flex bg-gray-50 w-full">
-  <img src={logo} on:click={()=>goto("/")} alt="logo" width={70} height={20} />
+  <img
+    src={logo}
+    on:click={() => goto("/")}
+    alt="logo"
+    width={70}
+    height={20}
+  />
 
-  <span class="justify-center items-center relative group md:flex hidden">
+  <span class="justify-center items-center relative group md:flex mt-[22px] md:mt-0">
     <button class=" hover:text-gray-400 md:ml-12 ml-2"> Who are we </button>
     <div
-      class="absolute left-0 hidden space-y-2 mt-48 bg-white text-black shadow-lg group-hover:block rounded-md w-48 p-2"
+      class="absolute left-0 hidden space-y-2 mt-6 md:mt-44 bg-white text-black shadow-lg group-hover:block rounded-md w-48 p-2"
     >
-      <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">About us</a>
-      <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Partner with us</a>
-      <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Resources</a>
+      <button
+        class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100"
+        on:click={() => {
+          const url = new URL(window.location.href);
+          if (url.pathname === "/contact-us") {
+            window.location.href = "/";
+            setTimeout(() => {
+              scrollTo({
+                top: 650,
+                behavior: "smooth",
+              });
+              console.log("hello")
+            }, 6000);
+          } else {
+            scrollTo({
+              top: 650,
+              behavior: "smooth",
+            });
+          }
+        }}>About us</button
+      >
+      <a
+        href="contact-us"
+        class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100"
+        >Partner with us</a
+      >
+      <!-- <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Resources</a> -->
     </div>
   </span>
-  <span class="justify-center items-center md:ml-20 ml-2 md:flex hidden"
-    ><a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">What we do</a></span
-  >
+  <!-- <span class="justify-center items-center md:ml-20 ml-2 md:flex hidden"
+    ><a href="/services" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">What we do</a></span
+  > -->
   <!-- <span class="flex justify-center items-center relative group md:ml-20 ml-2">
     <button class=" hover:text-gray-400"> Services </button>
     <div
@@ -49,10 +79,13 @@
       <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">SEO Optimization</a>
     </div>
   </span> -->
-  <span class="flex justify-center items-center md:ml-20 ml-2"
+  <!-- <span class="flex justify-center items-center md:ml-20 ml-2"
     ><a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Industries</a></span
-  >
+  > -->
   <span class="flex justify-center items-center md:ml-20 ml-2"
-    ><a href="/contact-us" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Contact us</a></span
+    ><a
+      href="/contact-us"
+      class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Contact us</a
+    ></span
   >
 </div>
---
+++ src/components/services.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import servicesImage from "../resources/images/services.jpg";
+</script>
+
+<div class="w-screen">
+    <div
+        class="w-full md:h-[400px] h-[200px] bg-no-repeat bg-cover flex justify-center items-center"
+        style="background-image: url({servicesImage})"
+    >
+        <h1 class="text-5xl text-white">Services</h1>
+    </div>
+</div>
